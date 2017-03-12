1929341bc0c64f47216a2f36e83b63ed750ca8d6
Daniel Creagh <daniel@creagh.com>
2017-03-12 14:21:13 +0000

    Initial Commit

---
+++ NEO_Early_Warning_System_Alpha_p1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NEO_Early_Warning_System_Alpha_p1
+//
+//  Created by Daniel Creagh on 12/03/2017.
+//  Copyright © 2017 Daniel Creagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
